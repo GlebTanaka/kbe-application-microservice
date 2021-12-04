845d8f8613c0d396c2a8aeffef12c91de45c7c09
GlebTanaka <gleb5655@gmail.com>
2021-12-04 16:02:57 +0100

    first commit

---
+++ src/main/java/de/htwberlin/f4/applicationmicroservice/ApplicationMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package de.htwberlin.f4.applicationmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/htwberlin/f4/applicationmicroservice/ApplicationMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.htwberlin.f4.applicationmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
