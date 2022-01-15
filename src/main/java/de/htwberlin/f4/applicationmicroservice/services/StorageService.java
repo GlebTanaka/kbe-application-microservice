f86c8a4768f0e6e85a5864bfb3a9e20af2c1a564
User5867 <lindi.f01@gmail.com>
2022-01-15 17:09:27 +0100

    add post Storage

--- src/main/java/de/htwberlin/f4/applicationmicroservice/services/StorageService.java
+++ src/main/java/de/htwberlin/f4/applicationmicroservice/services/StorageService.java
@@ -3,9 +3,14 @@ package de.htwberlin.f4.applicationmicroservice.services;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import de.htwberlin.f4.applicationmicroservice.services.storage.StorageObject;
+import okhttp3.FormBody;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 import okhttp3.ResponseBody;
+import okhttp3.FormBody.Builder;
+
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -52,5 +57,18 @@ public class StorageService {
         ObjectMapper objectMapper = new ObjectMapper();
         StorageObject storageObject = objectMapper.readValue(responseBody.string(), StorageObject.class);
         return storageObject;
+    }
+
+    public void postStorage(StorageObject storageObject) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+        RequestBody requestBody = new FormBody.Builder().add("id", storageObject.getId())
+            .add("amount", storageObject.getAmount().toString())
+            .add("place", storageObject.getPlace())
+            .add("duration", storageObject.getDuration().toString()).build();
+        Request request = new Request.Builder()
+                .url("http://localhost:8084/api/v1/storage/product/")
+                .post(requestBody)
+                .build();
+        Response response = client.newCall(request).execute();
     }
 }
