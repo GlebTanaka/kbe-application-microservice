353d62c1ab9288261acd7944b2a670ba92834160
GlebTanaka <gleb5655@gmail.com>
2022-01-20 20:27:50 +0100

    Add Dotenv file to store api key
    Including new dependency in pom.xml and modefing .gitignore file

--- src/main/java/de/htwberlin/f4/applicationmicroservice/services/GoogleMapsService.java
+++ src/main/java/de/htwberlin/f4/applicationmicroservice/services/GoogleMapsService.java
@@ -2,9 +2,11 @@ package de.htwberlin.f4.applicationmicroservice.services;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import de.htwberlin.f4.applicationmicroservice.models.googlemaps.GeocodeResult;
+import io.github.cdimascio.dotenv.Dotenv;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.ResponseBody;
+import org.jetbrains.annotations.Nullable;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -26,18 +28,24 @@ public class GoogleMapsService {
     public GeocodeResult getGeocode(String address) throws IOException {
         OkHttpClient client = new OkHttpClient();
         String encodedAddress = URLEncoder.encode(address, StandardCharsets.UTF_8);
-        //TODO extract API key as env variable from source
+        String apyKey = getApyKey();
         Request request = new Request.Builder()
                 .url("https://maps.googleapis.com/maps/api/geocode/json?address="
                         + encodedAddress
-                        // Manually extract api key for now, need to paste it in before running
-                        + "&key=")
+                        + "&key=" + apyKey)
                 .get()
                 .build();
         ResponseBody responseBody = client.newCall(request).execute().body();
         ObjectMapper objectMapper = new ObjectMapper();
         //TODO exception handling, if response equals null
         return objectMapper.readValue(responseBody.string(), GeocodeResult.class);
+    }
+
+    @Nullable
+    private String getApyKey() {
+        Dotenv dotenv;
+        dotenv = Dotenv.configure().load();
+        return dotenv.get("API_KEY");
     }
 }
 
